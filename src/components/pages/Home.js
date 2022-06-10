2726f4fe98f388b1595a136ba211b7df92d48b63
Mohamed Morsy <57285056+MohamedMorsy26@users.noreply.github.com>
2022-06-10 16:50:05 +0200

    front end gamed 5artella

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import axios from 'axios';
 
-const aesPassword="Secret Passphrase"
+const aesPassword = "Secret Passphrase"
 
 
 function Home() {
@@ -15,14 +15,17 @@ function Home() {
   };
 
   // Patient data
-  const [name,setName]=useState("")
-  const [age,setAge]=useState("")
-  const [blood,setBlood]=useState("")
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [gender, setGender] = useState("")
+  const [blood, setBlood] = useState("")
+  const [weight, setWeight] = useState("")
+  const [oxygen, setOxygen] = useState("")
 
-  const [publicKey,setPublicKey]=useState("")
-  const [privateKey,setPrivateKey]=useState("")
-  let priv=""
-  let pub=""
+  const [publicKey, setPublicKey] = useState("")
+  const [privateKey, setPrivateKey] = useState("")
+  let priv = ""
+  let pub = ""
 
   const [searchTerm, setSearchTerm] = useState('');
   const [patientList, setPatientList] = useState([]);
@@ -37,25 +40,25 @@ function Home() {
     //   .catch((err) => console.log(err.response.data));
   }, []);
 
-  const generateKeys=async()=>{
-    const options={
-      name:"RSASSA-PKCS1-v1_5",
-      modulusLength:4096,
-      publicExponent:new Uint8Array([0x01, 0x00, 0x01]),
-      hash:"SHA-512"
+  const generateKeys = async () => {
+    const options = {
+      name: "RSASSA-PKCS1-v1_5",
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: "SHA-512"
     }
-    let key=""
-    await window.crypto.subtle.generateKey(options, true,  ["sign","verify"] )
-    .then((res)=>{
-      console.log("KEYS")
-      console.log(res)
-      setPrivateKey(res.privateKey)
-      setPublicKey(res.publicKey)
-      priv=res.privateKey
-      pub=res.publicKey
-    }).catch((err)=>{
-      console.log(err)
-    })
+    let key = ""
+    await window.crypto.subtle.generateKey(options, true, ["sign", "verify"])
+      .then((res) => {
+        console.log("KEYS")
+        console.log(res)
+        setPrivateKey(res.privateKey)
+        setPublicKey(res.publicKey)
+        priv = res.privateKey
+        pub = res.publicKey
+      }).catch((err) => {
+        console.log(err)
+      })
     console.log("----")
     //! NEED TO SAVE KEY 
     // const ex=await window.crypto.subtle.exportKey("jwk",key.publicKey)
@@ -65,24 +68,24 @@ function Home() {
   }
 
 
-  const submit=async()=>{
+  const submit = async () => {
     await generateKeys()
-    const json={
-      name:name
+    const json = {
+      name: name
     }
 
-    const string=JSON.stringify(json)
-    const encrypted= await window.CryptoJS.AES.encrypt(string,aesPassword)
+    const string = JSON.stringify(json)
+    const encrypted = await window.CryptoJS.AES.encrypt(string, aesPassword)
 
     console.log(await encrypted.toString(window.CryptoJS.enc.Utf8))
     console.log(privateKey)
-    const buffer=new ArrayBuffer(encrypted);
-    const signed=await window.crypto.subtle.sign({ "name": "RSASSA-PKCS1-v1_5" },priv,buffer)
+    const buffer = new ArrayBuffer(encrypted);
+    const signed = await window.crypto.subtle.sign({ "name": "RSASSA-PKCS1-v1_5" }, priv, buffer)
     console.log("SIGNED")
     console.log(signed)
 
 
-    const verified=await window.crypto.subtle.verify({ "name": "RSASSA-PKCS1-v1_5" },pub,signed,buffer)
+    const verified = await window.crypto.subtle.verify({ "name": "RSASSA-PKCS1-v1_5" }, pub, signed, buffer)
     console.log("VERIFICATION")
     console.log(verified)
 
@@ -93,27 +96,42 @@ function Home() {
   }
 
   return (
-    <Container fluid>
-      <div
-        style={{
-          display: 'grid',
-          gridAutoRows: '50px',
-          gridTemplateColumns: '14fr 1fr',
-          margin: 0,
-        }}>
-          <div>
-        <h1> New patient</h1>
+    <div className='Container'>
+      <div>
         <div>
-          name
-          <input name='name' value={name} onChange={(value)=>setName(value.target.value)}/>
-          age
-          <input name='age' value={age} onChange={(value)=>setAge(value.target.value)}/>
-          5effetDam
-          <input name='5effed Dam' value={blood} onChange={(value)=>setBlood(value.target.value)}/>
-          <button type='button' onClick={submit}>  add record </button>
+          <h1> Add new patient</h1>
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Name:</label>
+              <input className="form-control" name='name' value={name} onChange={(value) => setName(value.target.value)}/>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Age:</label>
+              <input className="form-control" name='age' value={age} onChange={(value) => setAge(value.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Gender:</label>
+              <input className="form-control" name='gender' value={gender} onChange={(value) => setGender(value.target.value)}/>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Blood Type:</label>
+              <input className="form-control" name='blood' value={blood} onChange={(value) => setBlood(value.target.value)}/>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Weight:</label>
+              <input className="form-control" name='weight' value={weight} onChange={(value) => setWeight(value.target.value)}/>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Oxygen:</label>
+              <input className="form-control" name='oxygen' value={oxygen} onChange={(value) => setOxygen(value.target.value)}/>
+            </div>
+            <button type="button" onClick={submit} className="btn btn-primary">Add Record</button>
+          </form>
+          <div>
+            <button type='button' onClick={submit}>  add record </button>
+          </div>
         </div>
-        </div>
-        
+
         {/* Add Visit 
         in: patientid,data
         */}
@@ -121,8 +139,8 @@ function Home() {
         {/* Get all visits */}
         {/* Get visits for patientid (take patientid) */}
 
-        
-      </div> 
+
+      </div>
       <div
         style={{
           display: 'grid',
@@ -130,9 +148,9 @@ function Home() {
           alignItems: 'center',
           marginBottom: '50px',
         }}>
-        
+
       </div>
-    </Container>
+    </div>
   );
 }
 
